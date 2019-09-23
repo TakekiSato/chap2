874aa5dd1db9cb3c2ea78ff1d3627e2f8653c775
TakekiSato <takeyansaxplayer@gmail.com>
2019-09-24 00:26:57 +0900

    Initial Commit

---
+++ chap2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chap2
+//
+//  Created by TakekiSato on 2019/09/24.
+//  Copyright © 2019 TakekiSato. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
