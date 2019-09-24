bfb18b26ba105fa72b8f45bd15a080736a5064e9
TakekiSato <takeyansaxplayer@gmail.com>
2019-09-24 23:36:16 +0900

    SDL導入方法復習

--- chap2/main.cpp
+++ chap2/main.cpp
@@ -6,14 +6,23 @@
 //  Copyright © 2019 TakekiSato. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc,char *argv[]){
+    int close=0;
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_Window *window = SDL_CreateWindow("WindowTest",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,0);
+    
+    while(close==0){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+        }
+        if(event.type==SDL_QUIT)
+            close++;
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
-    
-    // Git Test
-    // Remote Repogitry 作成後 初Push
-    
 }
